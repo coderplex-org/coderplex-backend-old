3271379c17ef8362ff6a601f81cdfd3cf1a00ec5
teja <pbteja1998@gmail.com>
2017-11-23 19:11:37 +0530

    added some env vars

--- webapp/authentication/views.py
+++ webapp/authentication/views.py
@@ -2,13 +2,14 @@ from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.linkedin_oauth2.views import LinkedInOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from django.conf import settings
 
 class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
     client_class = OAuth2Client
-    callback_url = "http://127.0.0.1:8000/callback"
+    callback_url = settings.GITHUB_CALLBACK_URL
 
 class LinkedinLogin(SocialLoginView):
     adapter_class = LinkedInOAuth2Adapter
     client_class = OAuth2Client
-    callback_url = "http://127.0.0.1:8000/callback"
+    callback_url = settings.LINKEDIN_CALLBACK_URL
