1f3cc8f3a2c76f4d3597d773e447e40d72a1c06d
Vinay Puppal <me@vinaypuppal.com>
2017-12-01 01:43:47 +0530

    configure flake8 and pre-commit

--- webapp/manage.py
+++ webapp/manage.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
             import django
         except ImportError:
             raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
+                "Couldn't import Django. Are you sure it's installed"
+                "and available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
         raise
