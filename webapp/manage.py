22d43c0a99e702c18800b8d85f81b1a8da4163ee
Vinay Puppal <me@vinaypuppal.com>
2017-12-01 01:48:18 +0530

    add some more pre-commit hooks

--- webapp/manage.py
+++ webapp/manage.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed"
-                "and available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
+                "and available on your PYTHONPATH environment variable?"
+                "Did you forget to activate a virtual environment?"
             )
         raise
     execute_from_command_line(sys.argv)
